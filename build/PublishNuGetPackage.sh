80f87fff309ebcdec3fbd1c1c0aca6d9e39e61f1
Michiel van Oudheusden <m.vanoudheusden@gmail.com>
2014-04-27 23:52:57 +0200

    fix

--- build/PublishNuGetPackage.sh
+++ build/PublishNuGetPackage.sh
@@ -3,5 +3,5 @@ if [ "$TRAVIS_BRANCH" != "master" ]; then
 	NUGET_PACKAGE_NUM+="-${TRAVIS_BRANCH}${TRAVIS_BUILD_NUMBER}"
 fi
 sed -i "s/<version>.*<\/version>/<version>${NUGET_PACKAGE_NUM}<\/version>/g" Stateflow.nuspec
-mono --runtime=v4.0.30319 src/.nuget/NuGet.exe Pack Stateflow.nuspec -NonInteractive
-mono --runtime=v4.0.30319 src/.nuget/NuGet.exe Push Stateflow.${NUGET_PACKAGE_NUM}.nupkg $NUGET_APIKEY -NonInteractive
+mono --runtime=v4.0.30319 .nuget/NuGet.exe Pack Src/Stateflow/Stateflow.nuspec -NonInteractive
+mono --runtime=v4.0.30319 .nuget/NuGet.exe Push Src/Stateflow/Stateflow.${NUGET_PACKAGE_NUM}.nupkg $NUGET_APIKEY -NonInteractive
