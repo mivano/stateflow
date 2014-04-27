0c6418aba440ebdcf2daf8078ed05e13d0e1779b
Michiel van Oudheusden <m.vanoudheusden@gmail.com>
2014-04-27 23:34:31 +0200

    Added expressions.

---
+++ src/Stateflow.Expressions/ExpressionCondition.cs
@@ -0,0 +1,31 @@
+﻿using Expressions;
+using Stateflow.Workflow;
+
+namespace Stateflow.Expressions
+{
+    public class ExpressionCondition   : ICondition
+    {
+        public string Expression { get; set; }
+
+        public ExpressionCondition()
+        {
+            
+        }
+
+        public ExpressionCondition(string expression)
+        {
+            Expression = expression;
+        }
+
+        public virtual  bool IsAllowed(IWorkflow workflow)
+        {
+            var context = new ExpressionContext();
+
+            var expression = new DynamicExpression(Expression, ExpressionLanguage.Csharp);
+
+            var boundExpression = expression.Bind(context);
+
+            return (bool)boundExpression.Invoke(context); 
+        }
+    }
+}
