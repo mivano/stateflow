cedaee33dca6e4f823d46e8f5703f8b42b5a8fa4
lnai <lucian.naie@aerdata.com>
2014-07-22 18:17:37 +0200

    Fixed the sample app with a working workflow.

--- sample/LeadSample/Lead.cs
+++ sample/LeadSample/Lead.cs
@@ -6,18 +6,25 @@ using Stateflow.Fields.DataStores;
 
 namespace LeadSample
 {
-    public class Lead : WorkflowEntity 
-    {
-        public Lead(WorkflowDefinition workflowDefinition, string currentState) : base(workflowDefinition, currentState)
-        {
+	public class Lead : WorkflowEntity, IIdentifiableBy<string>
+	{
+		public Lead(WorkflowDefinition workflowDefinition, string currentState)
+			: base(workflowDefinition, currentState)
+		{
 			//Fields = new FieldCollection<string> ();
-        }
+		}
 
-        protected override void OnStateTransition(string fromState, string toState, string triggeredBy)
-        {
+		protected override void OnStateTransition(string fromState, string toState, string triggeredBy)
+		{
 			Console.WriteLine("{0}: State changed from {1} to {2} because of trigger {3}.", DateTime.Now.ToShortTimeString(), fromState, toState, triggeredBy);
-        }
+		}
 
 		public FieldCollection<string> Fields { get; set; }
-    }
+
+		public string Id
+		{
+			get;
+			set;
+		}
+	}
 }
--- sample/LeadSample/Program.cs
+++ sample/LeadSample/Program.cs
@@ -1,179 +1,206 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Stateflow.Fields;
 using Stateflow.Workflow;
 using Stateflow.Expressions;
 using Stateflow.Fields.DataStores;
 
 namespace LeadSample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var p = new Program();
-            p.CreateStateMachine();
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var p = new Program();
+			p.CreateStateMachine();
+		}
 
-   
 
-        public void CreateStateMachine()
-        {
-            var wd = new WorkflowDefinition
-            {
-                Name = "Test",
-                States = new List<State>
-                {
-                    new State
-                    {
-                        Name = "Created",
-                        DisplayName = "Lead Created"
-                    },
 
-                     new State
-                    {
-                        Name = "AwaitingReview",
-                        DisplayName = "Ready for Review"
-                    },
-                    new State
-                    {
-                        Name = "UnderReview",
-                        DisplayName = "Lead under Review"  ,
-                        EntryActions = new List<IAction>{new MailAction()}
-                    },
-                    new State
-                    {
-                        Name = "Approved",
-                        DisplayName = "Lead is Approved"
-                    } ,
-                     new State
-                    {
-                        Name = "Closed",
-                        DisplayName = "Lead is Closed"
-                    } 
-                },
-                Triggers = new List<Trigger>
-                {
-                     new Trigger
-                    {
-                        Name = "New"
-                    },
-                    new Trigger
-                    {
-                        Name = "Assign"
-                    },
+		public void CreateStateMachine()
+		{
+			var workflowDefinition = new WorkflowDefinition
+			{
+				Name = "Test",
+				States = new List<State>
+				{
+					new State
+					{
+						Name = "Created",
+						DisplayName = "Lead Created"
+					},
 
-                    new Trigger
-                    {
-                        Name = "Approve"
-                    }
-                    ,
-                    new Trigger
-                    {
-                        Name = "Reject"
-                    } ,
-                    new Trigger
-                    {
-                        Name="Close"
-                    }
-                },
-                Transitions = new List<Transition>
-                {
-                      new Transition
-                    {
-                         FromState = "Created",
-                         ToState = "AwaitingReview",
-                         TriggerBy = "Review"
-                    }  ,
-                    new Transition
-                    {
-                         FromState = "AwaitingReview",
-                         ToState = "UnderReview",
-                         TriggerBy = "Assign"
-                    }  ,
-                    
-                    new Transition
-                    {
-                         FromState = "UnderReview",
-                         ToState = "Approved",
-                         TriggerBy = "Approve"   ,
+					 new State
+					{
+						Name = "AwaitingReview",
+						DisplayName = "Ready for Review"
+					},
+					new State
+					{
+						Name = "UnderReview",
+						DisplayName = "Lead under Review"  ,
+						EntryActions = new List<IAction>{new MailAction()}
+					},
+					new State
+					{
+						Name = "Approved",
+						DisplayName = "Lead is Approved"
+					} ,
+					 new State
+					{
+						Name = "Closed",
+						DisplayName = "Lead is Closed"
+					} 
+				},
+				Triggers = new List<Trigger>
+				{
+					 new Trigger
+					{
+						Name = "New"
+					},
+					new Trigger
+					{
+						Name = "Assign"
+					},
+
+					new Trigger
+					{
+						Name = "Approve"
+					}
+					,
+					new Trigger
+					{
+						Name = "Reject"
+					} ,
+					new Trigger
+					{
+						Name="Close"
+					}
+				},
+				Transitions = new List<Transition>
+				{
+					  new Transition
+					{
+						 FromState = "Created",
+						 ToState = "AwaitingReview",
+						 TriggerBy = "Review"
+					}  ,
+					new Transition
+					{
+						 FromState = "AwaitingReview",
+						 ToState = "UnderReview",
+						 TriggerBy = "Assign"
+					}  ,
+					
+					new Transition
+					{
+						 FromState = "UnderReview",
+						 ToState = "Approved",
+						 TriggerBy = "Approve"   ,
 						Conditions = new List<ICondition>{ new LeadExpressionCondition("price > 10000")}
-                    } ,
-                      new Transition
-                    {
-                         FromState = "UnderReview",
-                         ToState = "Closed",
-                         TriggerBy = "Close"   ,
+					} ,
+					  new Transition
+					{
+						 FromState = "UnderReview",
+						 ToState = "Closed",
+						 TriggerBy = "Close"   ,
 						Conditions = new List<ICondition>{ new LeadExpressionCondition("price <= 10000")}
-                    } ,
-                     new Transition
-                    {
-                         FromState = "Approved",
-                         ToState = "Closed",
-                         TriggerBy = "Close"
-                    },
-                    new Transition
-                    {
-                         FromState = "UnderReview",
-                         ToState = "AwaitingReview",
-                         TriggerBy = "Reject"
-                    }
-                }
-            };
+					} ,
+					 new Transition
+					{
+						 FromState = "Approved",
+						 ToState = "Closed",
+						 TriggerBy = "Close"
+					},
+					new Transition
+					{
+						 FromState = "UnderReview",
+						 ToState = "AwaitingReview",
+						 TriggerBy = "Reject"
+					}
+				}
+			};
 
 			//var ser = wd.Serialize();
 
-            var lead = new Lead(wd, "Created")
-            {
-            };
+			var dataStore = new InMemoryDataStore<string>();
+			var lead = new Lead(workflowDefinition, "Created")
+			{
+				Id = Guid.NewGuid().ToString()
+			};
+			var field1Definition = new FieldDefinition<string>()
+			{
+				Id = Guid.NewGuid().ToString(),
+				Name = "ProductName",
+				FieldType = FieldType.SingleLineText,
+				DefaultValue = "Product Name",
+				IsEditable = true
+			};
+			var field2Definition = new FieldDefinition<string>()
+			{
+				Id = Guid.NewGuid().ToString(),
+				Name = "price",
+				FieldType = FieldType.Number,
+				DefaultValue = 0,
+				IsEditable = true
+			};
+			var defaultTemplate = new Template<string>(dataStore, "DefaultTemplate");
+			defaultTemplate.FieldDefinitions.Add(field1Definition);
+			defaultTemplate.FieldDefinitions.Add(field2Definition);
 
-//			lead.Fields.Add (new Field<string> {
-//				Name = "price",
-//				Value = 100000,
-//				FieldType = FieldType.Amount
-//			});
-//
-//			var assets = lead.Fields.AddCollection ("assets");
-//		
-//			assets.Add ("Name", "", FieldType.SingleLineText);
-//
-            while (lead.PermittedTriggers.Any())
-            {
+			var fieldsItem = defaultTemplate.CreateNew();
+			fieldsItem.Id = lead.Id;
+			fieldsItem.SetFieldValue(field1Definition, "Toyota iQ");
+			fieldsItem.SetFieldValue(field2Definition, 11000);
+			fieldsItem.Save();
 
-                Console.WriteLine("Current workflow state: {0}", lead.WorkflowState);
-                Console.WriteLine("  next available steps: {0}", String.Join(", ", lead.PermittedTriggers.ToArray()));
+			lead.Fields = fieldsItem.Fields;
 
-                Console.WriteLine("Enter the new state name:");
-                var nextStep = Console.ReadLine();
+			//var assets = lead.Fields.AddCollection("assets");
+			//assets.Add("Name", "", FieldType.SingleLineText);
 
-                if (string.IsNullOrWhiteSpace(nextStep))
-                    break;
+			while (lead.PermittedTriggers.Any())
+			{
 
-                lead.ChangeState(nextStep);
+				Console.WriteLine("Current workflow state: {0}", lead.WorkflowState);
+				Console.WriteLine("  next available steps: {0}", String.Join(", ", lead.PermittedTriggers.ToArray()));
 
-            }
+				Console.WriteLine("Enter the new state name:");
+				var nextStep = Console.ReadLine();
 
-            Console.WriteLine("No further steps, press a key to quit");
-            Console.ReadKey(true);
+				if (string.IsNullOrWhiteSpace(nextStep))
+					break;
 
-        }
-    }
+				lead.ChangeState(nextStep);
 
-	public class LeadExpressionCondition: ExpressionCondition{
+			}
 
-		public LeadExpressionCondition (string expression): base(expression)
+			Console.WriteLine("No further steps, press a key to quit");
+			Console.ReadKey(true);
+		}
+	}
+
+	public class LeadExpressionCondition : ExpressionCondition
+	{
+
+		public LeadExpressionCondition(string expression)
+			: base(expression)
 		{
-			
+
 		}
 
-		public override bool IsAllowed (IWorkflow workflow)
+		public override bool IsAllowed(IWorkflow workflow)
 		{
 			var leadWorkflow = workflow as Lead;
 			Dictionary<string, object> fields = null;
-			if (leadWorkflow != null) {
-				fields = leadWorkflow.Fields.Values.Select (a => new KeyValuePair<string, object> (a.Name, a.Value)).ToDictionary(a=>a.Key, b=>b.Value);
+			if (leadWorkflow != null)
+			{
+				fields = leadWorkflow.Fields.Values
+					.Select(a => new KeyValuePair<string, object>(a.Name, a.Value))
+					.ToDictionary(a => a.Key, b => b.Value);
 			}
-			return base.Evaluate (fields);
+			return base.Evaluate(fields);
 		}
 
 	}
